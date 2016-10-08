9531f6f722e8b243ec0d74fd0a24be40e2b8ca87
gioalvaro <gioalvaro@gmail.com>
2016-10-08 20:39:51 -0300

    Update plot2.R

--- Scripts/plot2.R
+++ Scripts/plot2.R
@@ -1,7 +1,7 @@
-household_power_consumption <- read.csv("household_power_consumption.txt", sep=";", na.strings="?", stringsAsFactors=FALSE)
-household_power_consumption <- household_power_consumption[(household_power_consumption$Date == '1/2/2007')|(household_power_consumption$Date == '2/2/2007'),]
-household_power_consumption$Date <- as.Date(household_power_consumption$Date, "%d/%m/%Y")
-household_power_consumption$DateTime <- as.POSIXct(paste(household_power_consumption$Date, household_power_consumption$Time), format="%Y-%m-%d %H:%M:%S")
-with(household_power_consumption, plot(DateTime,Global_active_power, type='l', ylab='Global Active Power (kilowatts)', xlab=''))
+household.power.consumption <- read.csv("household_power_consumption.txt", sep=";", na.strings="?", stringsAsFactors=FALSE)
+household.power.consumption <- household.power.consumption[(household.power.consumption$Date == '1/2/2007')|(household.power.consumption$Date == '2/2/2007'),]
+household.power.consumption$Date <- as.Date(household.power.consumption$Date, "%d/%m/%Y")
+household.power.consumption$DateTime <- as.POSIXct(paste(household.power.consumption$Date, household.power.consumption$Time), format="%Y-%m-%d %H:%M:%S")
+with(household.power.consumption, plot(DateTime,Global_active_power, type='l', ylab='Global Active Power (kilowatts)', xlab=''))
 dev.copy(png,'plot2.png')
 dev.off()
