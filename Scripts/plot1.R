a07de944bbc5e52a9c5412440b5f7b9c7eb39f35
gioalvaro <gioalvaro@gmail.com>
2016-10-09 10:27:28 -0300

    Update plot1.R

--- Scripts/plot1.R
+++ Scripts/plot1.R
@@ -1,5 +1,5 @@
 household.power.consumption <- read.csv("household_power_consumption.txt", sep=";", na.strings="?", stringsAsFactors=FALSE)
 household.power.consumption <- household.power.consumption[(household.power.consumption$Date == '1/2/2007')|(household.power.consumption$Date == '2/2/2007'),]
 hist(household.power.consumption$Global_active_power,xlab='Global Active Power (kilowatts)',main='Global Active Power',col='red')
-dev.copy(png,'plot1.png')
+dev.copy(png,'plot1.png',width=480,height=480,units='px')
 dev.off()
