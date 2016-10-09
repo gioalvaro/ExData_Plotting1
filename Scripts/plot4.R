2f5ec0b3193cde5ad03175e16a39a5839a7bb611
gioalvaro <gioalvaro@gmail.com>
2016-10-09 10:28:12 -0300

    Update plot4.R

--- Scripts/plot4.R
+++ Scripts/plot4.R
@@ -12,5 +12,5 @@ with(household.power.consumption,{
   legend(x="topright",legend=c('Sub_metering_1','Sub_metering_2','Sub_metering_3'), lty=1, col=c('black','red','blue'),bty='n')
   plot(DateTime,Global_reactive_power,type='l',ylab='Global_reactive_power',xlab='datetime')
 })
-dev.copy(png,'plot4.png')
+dev.copy(png,'plot4.png',width=480,height=480,units='px')
 dev.off()
