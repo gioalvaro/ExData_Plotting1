0287d8f3d418d3cfaa233207d0d7e41cb338616a
gioalvaro <gioalvaro@gmail.com>
2016-10-09 10:27:59 -0300

    Update plot3.R

--- Scripts/plot3.R
+++ Scripts/plot3.R
@@ -8,5 +8,5 @@ with(household.power.consumption,{
   lines(DateTime,Sub_metering_3,type='l',col='blue')
   legend('topright',c('Sub_metering_1','Sub_metering_2','Sub_metering_3'), lty=1, col=c('black','red','blue'), bty='n')
 })
-dev.copy(png,'plot3.png')
+dev.copy(png,'plot3.png',width=480,height=480,units='px')
 dev.off()
